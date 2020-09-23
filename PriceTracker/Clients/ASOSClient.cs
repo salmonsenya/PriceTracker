df637a53501bfa75310f2ec8a76646833cfa5c55
Kseniia Khromova <kseniiakhromova@yandex.ru>
2020-09-23 22:40:38 +0300

    ASOSClient prototype added

---
+++ PriceTracker/Clients/ASOSClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PriceTracker.Clients
+{
+    public class ASOSClient : IASOSClient
+    {
+        private HttpClient _httpClient;
+
+        public ASOSClient(HttpClient httpClient, IOptions<ASOSApiOptions> options)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+
+    }
+}
---
+++ PriceTracker/ServiceCollectionExtention.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PriceTracker.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceTracker
+{
+    public static class ServiceCollectionExtention
+    {
+        public static void AddHttpClients(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpClient<IASOSClient, ASOSClient>();
+        }
+    }
+}
